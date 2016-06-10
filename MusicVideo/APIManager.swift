74106c181daedac777875671abbe9c24a3306d53
Kevin <rkevinr@kevins-macbook-pro.local>
2016-06-10 11:05:53 -0500

    initial JSON serialization steps

--- MusicVideo/APIManager.swift
+++ MusicVideo/APIManager.swift
@@ -11,26 +11,39 @@ import Foundation
 class APIManager {
     
     func loadData(urlString: String, completion: (result: String) -> () ) {
-        
         // preclude session caching
         let config = NSURLSessionConfiguration.ephemeralSessionConfiguration()
         let session = NSURLSession(configuration: config)
-        
         let url = NSURL(string: urlString)!
         
         let task = session.dataTaskWithURL(url) {
             (data, response, error) -> () in
             
-            dispatch_async(dispatch_get_main_queue()) {
-                if error != nil {
+            guard error == nil else {
+                dispatch_async(dispatch_get_main_queue()) {
                     completion(result: (error!.localizedDescription))
-                } else {
-                    completion(result: "NSURLSession successful")
-                    print(data!)
+                }
+                return  // should this be break vs. return?
+            }
+            
+            do {
+                if let json = try NSJSONSerialization.JSONObjectWithData(
+                data!, options: .AllowFragments) as? [String: AnyObject] {
+                    print(json)
+                    
+                    let priority = DISPATCH_QUEUE_PRIORITY_HIGH
+                    dispatch_async(dispatch_get_global_queue(priority, 0)) {
+                        dispatch_async(dispatch_get_main_queue()) {
+                            completion(result: "JSONSerialization successful")
+                        }
+                    }
+                }
+            } catch {
+                dispatch_async(dispatch_get_main_queue()) {
+                    completion(result: "error in JSONSerialization")
                 }
             }
         }
-        
         task.resume()
     }
 }
